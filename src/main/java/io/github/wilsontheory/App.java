ef34d8b3dd4ce480b069773c64604279e3e7d24b
wilsontheory <wilson.brian90@gmail.com>
2017-12-03 23:58:54 -0500

    Add pointcut examples

--- src/main/java/io/github/wilsontheory/App.java
+++ src/main/java/io/github/wilsontheory/App.java
@@ -15,6 +15,7 @@ public class App
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
     	CatService catService = context.getBean("catService", CatService.class);
     	System.out.println(catService.getRussianBlue().getName());
+    	System.out.println(catService.getSiamese().getName());
         System.out.println( "Hello World!" );
     }
 }
--- src/main/java/io/github/wilsontheory/aspect/LoggingAspect.java
+++ src/main/java/io/github/wilsontheory/aspect/LoggingAspect.java
@@ -1,14 +1,46 @@
 package io.github.wilsontheory.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
-	public void LoggingAdvice(){
-		//Advices refer to methods in an Aspect
-		System.out.println("Advice run. Get method called");
+//	@Before("execution(public String getName())") executes for all that match this method signature
+	@Before("execution(public String io.github.wilsontheory.model.RussianBlue.getName())") 
+	//Advice only runs for RussianBlue.getName(), these are 'Point Cuts'
+	public void loggingAdvice(){
+		//Advices refer to methods (cross-cutting concerns) in an Aspect
+		System.out.println("Advice run for RussianBlue. Get method called");
 	}
+	
+	@After("execution(* get*())") //wildcards in/as return type and method name
+	//add * in method sig arg for 1 or more arg of any kind, add .. for 0 or more
+	public void loggingAdviceGetters(){
+		System.out.println("A getter method was just run!");
+	}
+	
+	@Before("siameseGetters()")
+	public void printBeforeSetter(){
+		System.out.println("Advice to print before getting the siamese name");
+	}
+	
+	@Before("siameseGetters() && allSiameseMethodsExample()") //good practice
+	public void printCombinedSiamese(){
+		System.out.println("Advice triggered before Siamese getter AND siamese method");
+	}
+	
+	@Pointcut("execution(* io.github.wilsontheory.*.Siamese.getName(..))")
+	public void siameseGetters(){}
+	
+	@Pointcut("execution(* io.github.wilsontheory.model.Siamese.*(..))")
+	public void allSiameseMethodsExample(){};
+	//same as
+	@Pointcut("within(io.github.wilsontheory.model.Siamese)")
+	public void allSiamesePointcutWithinExample(){};
+	
+//	@Pointcut("within(io.github.wilsontheory.model..*") would include all subpackages within model
+
 }
