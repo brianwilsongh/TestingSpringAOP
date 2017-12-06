c7e8471e5bc2519bbbf3b30a05566e38b03d5341
wilsontheory <wilson.brian90@gmail.com>
2017-12-06 00:01:01 -0500

    Add XML configured aspect with an advice

---
+++ src/main/java/io/github/wilsontheory/aspect/LoggingAspectXML.java
@@ -0,0 +1,8 @@
+package io.github.wilsontheory.aspect;
+
+public class LoggingAspectXML {
+
+	public void printLogFromXMLConfig(){
+		System.out.println("XML configured aspect printed this line! (runs before all getters)");
+	}
+}
